b0a92750ba8a3daef771b11a0bc5c4be9d0098f4
Raul Sigoli <raul_sigoli@hotmail.com>
2023-02-09 11:43:55 -0300

    feat: Documentação de tokens

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Button, ButtonProps } from '@ignite-ui/react'
 
 export default {
-  title: 'Button',
+  title: 'From/Button',
   component: Button,
 
   args: {
