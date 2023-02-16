1dd05aeaa44c543877f0492ed8f7188318318de2
Raul Sigoli <raul_sigoli@hotmail.com>
2023-02-16 10:03:45 -0300

    no pc da firma roda no de casa nao, entao vai ser com todas as dependências juntas

---
+++ node_modules/@ignite-ui/docs/storybook-static/assets/preview.dd406d2e.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.375fd60f.js";import"./iframe.d3a0f484.js";import"./es.map.constructor.270224c6.js";import"./es.number.to-fixed.d5c9ba00.js";import"./index.00377d14.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.dd406d2e.js.map
