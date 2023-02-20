1b29281c26f33bb6376342c37c500195303ed57c
Raul Sigoli <raul_sigoli@hotmail.com>
2023-02-20 17:10:58 -0300

    feat: Controles nos componentes

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/rauzola.png',
     alt: 'Raul Sigoli',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
