933f06a784bd2e8ecabd9a6c3afbd3b2756a3f68
Raul Sigoli <raul_sigoli@hotmail.com>
2023-02-21 22:17:21 -0300

    fix: npm run build

--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Box, BoxProps, Text } from '@ignite-ui/react'
+import { Box, BoxProps, Text } from '@rauzola-ignite-ui/react'
 
 export default {
   title: 'From/Box',
